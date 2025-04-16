a5bc1f1f4272ea4aa658d3d4edc6322df28dac1f
aliyun-computenest <aliyun-computenest@alibaba-inc.com>
2025-04-16 18:47:00 +0800

    update

--- mcp/prepare.sh
+++ mcp/prepare.sh
@@ -8,12 +8,14 @@ export NVM_DIR="$HOME/.nvm"
 # 安装node，以node版本22为例
 nvm install 22
 
-pip install --upgrade pip && \
-    pip install --no-cache-dir \
+uv pip install --upgrade pip && \
+    uv pip install --no-cache-dir \
         "httpx<0.28" \
         "markdownify>=0.13.1" \
         "mcp>=1.1.3" \
         "protego>=0.3.1" \
         "pydantic>=2.0.0" \
         "readabilipy>=0.2.0" \
-        "requests>=2.32.3"
+        "requests>=2.32.3" \
+        "mcp_server_fetch" \
+        "mcp-server-time"
