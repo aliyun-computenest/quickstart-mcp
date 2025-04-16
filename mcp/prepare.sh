367dca5e4426ee9ea3710d68b20189bcc0c07e71
bupt_rain <lyh376240@alibaba-inc.com>
2025-04-16 18:39:58 +0800

    update

--- mcp/prepare.sh
+++ mcp/prepare.sh
@@ -1,11 +1,4 @@
-# 安装python
-yum install python3.11 -y
-# 创建python虚拟环境
-python3.11 -m venv venv
-# 启动虚拟环境
-source venv/bin/activate
-# 安装mcpo
-pip install mcpo
+
 # 下载nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 # 配置nvm
@@ -14,3 +7,13 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 # 安装node，以node版本22为例
 nvm install 22
+
+pip install --upgrade pip && \
+    pip install --no-cache-dir \
+        "httpx<0.28" \
+        "markdownify>=0.13.1" \
+        "mcp>=1.1.3" \
+        "protego>=0.3.1" \
+        "pydantic>=2.0.0" \
+        "readabilipy>=0.2.0" \
+        "requests>=2.32.3"
